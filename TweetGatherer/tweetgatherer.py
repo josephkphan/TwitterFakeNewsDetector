7d9cc65687e51462c2a93e37d5f6b24b0ae8c695
Joey <roophanxp@gmail.com>
2017-01-09 13:52:20 -0800

    find out information about users given username

--- TweetGatherer/tweetgatherer.py
+++ TweetGatherer/tweetgatherer.py
@@ -1,3 +1,4 @@
+import tweepy
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
@@ -13,11 +14,17 @@ accessSecret = key.accessSecret
 
 # Creating Listener class that will gather data from API
 class Listener(StreamListener):
-
     # Authentication worked and getting data
     def on_data(self, data):
         try:
+            tweet = data.split(',"text":"')[1].split('","source')[0]
+            userID = data.split(',"id":')[1].split(',"id_str')[0]
+            timeStamp = str(time.time())
             print data
+            print 'TimeStamp: ' + timeStamp
+            print 'Text: ' + tweet
+            print 'userID: ' + userID
+            print '----------------------'
             return True
 
         # Could happen with bad internet... etc other errors
@@ -28,11 +35,27 @@ class Listener(StreamListener):
     # Happens when an error occurs - probably through a wrong key
     def on_error(self, status):
         print status
+        if status == 420:  # 420 means maxed out on number of requests in a window of time
+            # returning False in on_data disconnects the stream
+            return False
+
 
 # Setting Authentication Keys for API
 auth = OAuthHandler(consumerKey, consumerSecret)
+api = tweepy.API(auth)
 auth.set_access_token(accessToken, accessSecret)
 twitterStream = Stream(auth, Listener())
 
 # Gathering tweets with keyword Trump
-twitterStream.filter(track=["Trump"])
+# twitterStream.filter(track=["Trump"])
+users = [818568080536178688, 0]
+# username = api.lookup_users(users)
+# print username
+
+user_data = api.get_user('DylantheOstrich')
+print 'Followers: ' + str(user_data.followers_count)
+print 'Tweets: ' + str(user_data.statuses_count)
+print 'Favourites: ' + str(user_data.favourites_count)
+print 'Friends: ' + str(user_data.friends_count)
+print 'Appears on ' + str(user_data.listed_count) + ' lists'
+# 818568080536178688 DYLANS ID
