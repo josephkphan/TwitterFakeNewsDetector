9be134b13a3816fc5c0ce7a529f45baa3142b1f6
Joey <roophanxp@gmail.com>
2017-01-30 09:16:55 -0800

    clean

--- TweetGatherer/key.py
+++
@@ -1,7 +0,0 @@
-consumerKey = 'eOOgZZpX8hmaPxIn0VoRbECil'
-consumerSecret = '7rLhBiVALul0IYKcqbx0DRBajzPtYofO5Iw2Szv25rWhqmXFkW'
-accessToken = '818157828921667584-6cTUK2RvwKdjpRevQ7msUE22sLcUUJi'
-accessSecret = 'SZBZKw10OAINbqQaCp5ZzUgQDXRPNkI2UhHDACmwafdIO'
-
-tweetNum=1
-curFileNum=0
