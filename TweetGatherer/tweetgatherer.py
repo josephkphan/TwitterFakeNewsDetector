c5564ae884fc36aeaf1987b028737a6d8b8b9268
Joey <roophanxp@gmail.com>
2017-01-29 23:25:58 -0800

    find out information about users given username

--- TweetGatherer/tweetgatherer.py
+++ TweetGatherer/tweetgatherer.py
@@ -4,6 +4,7 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import key
 import time
+import json
 
 # ------------Keys for API---------- #
 consumerKey = key.consumerKey
@@ -15,16 +16,29 @@ accessSecret = key.accessSecret
 # Creating Listener class that will gather data from API
 class Listener(StreamListener):
     # Authentication worked and getting data
-    def on_data(self, data):
+    def on_data(self, str_data):
         try:
-            tweet = data.split(',"text":"')[1].split('","source')[0]
-            userID = data.split(',"id":')[1].split(',"id_str')[0]
-            timeStamp = str(time.time())
-            print data
-            print 'TimeStamp: ' + timeStamp
-            print 'Text: ' + tweet
-            print 'userID: ' + userID
-            print '----------------------'
+            # data from on_data initially comes in as string. Converts to json
+            print str_data
+            data = json.loads(str_data)
+
+            # Checking if keys exist in data
+            if 'id' in data and 'text' in data and 'user' in data:
+                try:
+
+                    saveFile = open('TrumpData.txt', 'a')
+                    saveFile.write(str_data)
+                    saveFile.close()
+                    return True
+
+                except BaseException, e:
+                    print 'failed OnData: ', str(e)
+                    time.sleep(5)
+
+            else:
+                # Data does not exist. Ignore this case
+                print "information does not exist"
+
             return True
 
         # Could happen with bad internet... etc other errors
@@ -32,12 +46,19 @@ class Listener(StreamListener):
             print 'failed on_data,', str(e)
             time.sleep(5)
 
+        def print_data():
+            print data['id']
+            print data['text']
+            print data['user']['name']
+            print data['user']['screen_name']
+
     # Happens when an error occurs - probably through a wrong key
     def on_error(self, status):
         print status
         if status == 420:  # 420 means maxed out on number of requests in a window of time
             # returning False in on_data disconnects the stream
             return False
+        time.sleep(30)
 
 
 # Setting Authentication Keys for API
@@ -47,15 +68,18 @@ auth.set_access_token(accessToken, accessSecret)
 twitterStream = Stream(auth, Listener())
 
 # Gathering tweets with keyword Trump
-# twitterStream.filter(track=["Trump"])
-users = [818568080536178688, 0]
+twitterStream.filter(track=["Trump"])
+
+
+# ---------gathering information on users via username ------------ #
+# users = [818568080536178688, 0]
 # username = api.lookup_users(users)
 # print username
 
-user_data = api.get_user('DylantheOstrich')
-print 'Followers: ' + str(user_data.followers_count)
-print 'Tweets: ' + str(user_data.statuses_count)
-print 'Favourites: ' + str(user_data.favourites_count)
-print 'Friends: ' + str(user_data.friends_count)
-print 'Appears on ' + str(user_data.listed_count) + ' lists'
-# 818568080536178688 DYLANS ID
+# user_data = api.get_user('DylantheOstrich')
+# print 'Followers: ' + str(user_data.followers_count)
+# print 'Tweets: ' + str(user_data.statuses_count)
+# print 'Favourites: ' + str(user_data.favourites_count)
+# print 'Friends: ' + str(user_data.friends_count)
+# print 'Appears on ' + str(user_data.listed_count) + ' lists'
+# 818568080536178688 DYLANS IDa
