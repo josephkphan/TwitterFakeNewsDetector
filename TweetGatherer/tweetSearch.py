5615870404aad88dbf481ea95ba69eca8d17c7ab
josephkphan <josephkphan@gmail.com>
2017-02-22 10:09:28 -0800

    Added Excel write functionality and organized code

--- TweetGatherer/tweetSearch.py
+++ TweetGatherer/tweetSearch.py
@@ -7,7 +7,19 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import data
 import dataLoader
+import xlwt
 
+# ------------------ What you want to Find -------------------- #
+# How to use Keywords:
+# To input multiple phrases, Separate your strings with a commas
+# ex: "Car" will find anything with
+# ex: "Red Car" Will find anything with red and car in that exact format (i.e. This is a red car)
+# ex: "Red, Car" Will find anything with Red and Car in the same sentence (i.e. I have a Car. I have a Red Apple)
+keywords = "Trump, insult, Disabilities"
+excelSheetName = "data.xls"
+# textFileName = "data.txt"
+
+# ---------------- Access Keys ------------------#
 consumerKey = data.key['consumerKey']
 consumerSecret = data.key['consumerSecret']
 accessToken = data.key['accessToken']
@@ -17,15 +29,8 @@ auth = OAuthHandler(consumerKey, consumerSecret)
 api = tweepy.API(auth)
 auth.set_access_token(accessToken, accessSecret)
 
-# Use csv Writer
-# fileString = 'testing.txt'
-# saveFile = open(fileString, 'a')
-# saveFile.write(str_data)
-# saveFile.close()
-fileString = 'testing.txt'
-saveFile = open(fileString, 'a')
 
-
+# ------------ Tweet Extraction Functions ---------- #
 def get_text(string):
     buffer = string.split("text=")
     text = buffer[1].split(", ")
@@ -80,14 +85,45 @@ def get_retweet_count(string):
     return text[0]
 
 
+# ------------ Using Text File ------------- $
+# Use csv Writer
+# saveFile = open(fileString, 'a')
+# saveFile.write(str_data)
+# saveFile.close()
+# fileString = 'testing.txt'
+# saveFile = open(fileString, 'a')
+
+# ----------- Using Excel Sheet ------------ $
+book = xlwt.Workbook(encoding="utf-8")
+sheet1 = book.add_sheet("Sheet 1")
+
+sheet1.write(0, 0, "Tweet")
+sheet1.write(0, 1, "Verified")
+sheet1.write(0, 2, "Irrelevant")
+sheet1.write(0, 3, "Username")
+sheet1.write(0, 4, "userID")
+sheet1.write(0, 5, "Tweet Length")
+sheet1.write(0, 6, "Retweet Count")
+sheet1.write(0, 7, "Friends Count")
+sheet1.write(0, 8, "Followers Count")
+sheet1.write(0, 9, "Status Count")
+sheet1.write(0, 10, "Favorites Count")
+sheet1.write(0, 11, "List Count")
+
+# ------------ Iterating Through Tweets ------------ #
+counter = 1
 for tweet in tweepy.Cursor(api.search,
-                           q="Trump, hillary",
-                           since="2017-01-29",
-                           until="2017-01-30",
+                           q= keywords,
+                           since="2017-02-10",
+                           until="2017-02-22",
                            lang="en").items(10000):
-    # Write a row to the csv file/ I use encode utf-8
-    # print tweet
     tweet_string = str(tweet)
+
+    # Used to Skip "Reply Tweets"
+    # if tweet_string.find('RT') > 0:         # Returns index if found. Returns -1 if not found
+    #     continue
+
+    # Parsing through Tweet for data
     tweet_data = {}
     tweet_data['text'] = get_text(tweet_string)
     tweet_data['retweet_count'] = get_retweet_count(tweet_string)
@@ -98,7 +134,29 @@ for tweet in tweepy.Cursor(api.search,
     tweet_data['status_count'] = get_status_count(tweet_string)
     tweet_data['list_count'] = get_list_count(tweet_string)
     tweet_data['followers_count'] = get_followers_count(tweet_string)
-    print tweet_data
+
+    # Writing Data to Excel Sheet
+    sheet1.write(counter, 0, tweet_data['text'])
+    sheet1.write(counter, 3, tweet_data['screen_name'])
+    sheet1.write(counter, 4, tweet_data['id_str'])
+    sheet1.write(counter, 5, len(tweet_data['text'].split()))
+    sheet1.write(counter, 6, tweet_data['retweet_count'])
+    sheet1.write(counter, 7, tweet_data['friends_count'])
+    sheet1.write(counter, 8, tweet_data['followers_count'])
+    sheet1.write(counter, 9, tweet_data['status_count'])
+    sheet1.write(counter, 10, tweet_data['favorite_count'])
+    sheet1.write(counter, 11, tweet_data['list_count'])
+    counter += 1
+
+    # Writing to Text File
     # saveFile.write(str(tweet))
     # saveFile.write('\n')
-saveFile.close()
+
+    # Print out to Console
+    print tweet_data
+    print '------------------------------------------------------'
+    # print tweet_data['text']
+    # print len(tweet_data['text'].split())
+
+book.save(excelSheetName)
+# saveFile.close()
